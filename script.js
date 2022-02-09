9eda4524a4dd24bb1d7442fa66d70fc48d3f69bd
Yonraz <yraz530@gmail.com>
2022-02-09 12:03:11 +0200

    add generateChord function

--- script.js
+++ script.js
@@ -1,14 +1,20 @@
 const scaleTones = ['C', 'D', 'E', 'F', 'G', 'A', 'B'];
+
 function randomizeNum(array){
     let randNum = Math.floor(Math.random()* array.length);
-    return randNum
+    return randNum;
 }
 const generateScale = () =>{
     const possibleTonality = ['Major', 'Minor', 'Phrygian', 'Lydian', 'Locrian', 'Mixolydian', 'Dorian'];
-    return scaleTones[randomizeNum(scaleTones)] + " " + possibleTonality[randomizeNum(possibleTonality)]
+    return scaleTones[randomizeNum(scaleTones)] + " " + possibleTonality[randomizeNum(possibleTonality)];
 }
 
 const generateChord = () =>{
-    const possibleChordType = []
+    const possibleChordType = ['', 'maj', 'min', 'dim', 'aug'];
+    const possibleTensions = ['7', 'maj7', 'maj9' , 'sus4', 'min9', '#11', 'alt']
+    if(possibleChordType[randomizeNum(possibleChordType)] === ''){
+    return scaleTones[randomizeNum(scaleTones)] +  possibleTensions[randomizeNum(possibleTensions)]
+    }
+    return scaleTones[randomizeNum(scaleTones)] +  possibleChordType[randomizeNum(possibleChordType)]
 }
-console.log(generateScale())
+
