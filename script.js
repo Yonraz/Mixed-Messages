091c7f5def1421d82fe356173f6855a2159a3c8a
Yonraz <yraz530@gmail.com>
2022-02-09 13:17:59 +0200

    add message generation functions

--- script.js
+++ script.js
@@ -12,15 +12,45 @@ const generateScale = () =>{
 
 const generateChord = () =>{
     const possibleChordType = ['', 'maj', 'min', 'dim', 'aug'];
-    const possibleTensions = ['7', 'maj7', 'maj9' , 'sus4', 'min9', '#11', 'alt']
+    const possibleTensions = ['7', 'maj7', 'maj9' , 'sus4', 'min9', '#11', 'alt7'];
     if(possibleChordType[randomizeNum(possibleChordType)] === ''){
-    return scaleTones[randomizeNum(scaleTones)] +  possibleTensions[randomizeNum(possibleTensions)]
+    return scaleTones[randomizeNum(scaleTones)] +  possibleTensions[randomizeNum(possibleTensions)];
     }
-    return scaleTones[randomizeNum(scaleTones)] +  possibleChordType[randomizeNum(possibleChordType)]
+    return scaleTones[randomizeNum(scaleTones)] +  possibleChordType[randomizeNum(possibleChordType)];
 }
 
 const generateTimeSignature = () =>{
-    const signatureArray = ['4/4', '3/4', '5/4', '7/8']
-    return signatureArray[randomizeNum(signatureArray)]
+    const signatureArray = ['4/4', '3/4', '5/4', '7/8'];
+    return signatureArray[randomizeNum(signatureArray)];
 }
 
+let randScale = generateScale();
+let randChord = generateChord();
+let randTimeSignature = generateTimeSignature();
+
+const generateScaleMessage = (scale) =>{
+    const message = [`Your song is in the scale of ${scale}.`, `You better use ${scale} for this one.`, `${scale} is your scale for today.`];
+    return message[randomizeNum(message)];
+}
+
+const generateChordMessage = (chord) =>{
+    const message = [`Play ${chord}.`, `${chord} might be a good choice.`, `Try ${chord}! I swear it sounds good.`];
+    return message[randomizeNum(message)];
+}
+
+const generateTSMessage = (timeSig) =>{
+    const message = [`Your song should be in ${timeSig}... if you can handle it.`, `Maybe try doing it in ${timeSig} time`, `Your chosen time signature is ${timeSig}.`];
+    return message[randomizeNum(message)];
+}
+
+
+console.log(generateTSMessage(randTimeSignature))
+
+
+// const getMessage = (scale, chord, TS) => {
+    // const typeOfMessage = [scale, chord, TS];
+    // let subject = typeOfMessage[randomizeNum(typeOfMessage)]
+    // if (subject === scale) {
+        // return 
+    // } 
+// }
