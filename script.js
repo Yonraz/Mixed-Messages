3bfa8c99fa54e632297087ee7f6e308cb653ec1b
Yonraz <yraz530@gmail.com>
2022-02-09 23:55:58 +0200

    Change randomizeNumber function name

--- script.js
+++ script.js
@@ -1,27 +1,27 @@
 const scaleTones = ['C', 'D', 'E', 'F', 'G', 'A', 'B'];
 
-function randomizeNum(array){
+function getRandomNumber(array){
     let randNum = Math.floor(Math.random()* array.length);
     return randNum;
 }
 
 const generateScale = () =>{
     const possibleTonality = ['Major', 'Minor', 'Phrygian', 'Lydian', 'Locrian', 'Mixolydian', 'Dorian'];
-    return scaleTones[randomizeNum(scaleTones)] + " " + possibleTonality[randomizeNum(possibleTonality)];
+    return scaleTones[getRandomNumber(scaleTones)] + " " + possibleTonality[getRandomNumber(possibleTonality)];
 }
 
 const generateChord = () =>{
     const possibleChordType = ['', 'maj', 'min', 'dim', 'aug'];
     const possibleTensions = ['7', 'maj7', 'maj9' , 'sus4', 'min9', '#11', 'alt7'];
-    if(possibleChordType[randomizeNum(possibleChordType)] === ''){
-    return scaleTones[randomizeNum(scaleTones)] +  possibleTensions[randomizeNum(possibleTensions)];
+    if(possibleChordType[getRandomNumber(possibleChordType)] === ''){
+        return scaleTones[getRandomNumber(scaleTones)] +  possibleTensions[getRandomNumber(possibleTensions)];
     }
-    return scaleTones[randomizeNum(scaleTones)] +  possibleChordType[randomizeNum(possibleChordType)];
+    return scaleTones[getRandomNumber(scaleTones)] +  possibleChordType[getRandomNumber(possibleChordType)];
 }
 
 const generateTimeSignature = () =>{
     const signatureArray = ['4/4', '3/4', '5/4', '7/8'];
-    return signatureArray[randomizeNum(signatureArray)];
+    return signatureArray[getRandomNumber(signatureArray)];
 }
 
 let randScale = generateScale();
@@ -30,22 +30,22 @@ let randTimeSignature = generateTimeSignature();
 
 const generateScaleMessage = (scale) =>{
     const message = [`Your song is in the scale of ${scale}.`, `You better use ${scale} for this one.`, `${scale} is your scale for today.`];
-    return message[randomizeNum(message)];
+    return message[getRandomNumber(message)];
 }
 
 const generateChordMessage = (chord) =>{
     const message = [`Play ${chord}.`, `${chord} might be a good choice.`, `Try ${chord}! I swear it sounds good.`];
-    return message[randomizeNum(message)];
+    return message[getRandomNumber(message)];
 }
 
 const generateTSMessage = (timeSig) =>{
     const message = [`Your song should be in ${timeSig}... if you can handle it.`, `Maybe try doing it in ${timeSig} time`, `Your chosen time signature is ${timeSig}.`];
-    return message[randomizeNum(message)];
+    return message[getRandomNumber(message)];
 }
 
 const getFinalMessage = (scaleFunc, chordFunc, TSFunc) =>{
     const possibleMessageArray = [scaleFunc, chordFunc, TSFunc];
-    return possibleMessageArray[randomizeNum(possibleMessageArray)];
+    return possibleMessageArray[getRandomNumber(possibleMessageArray)];
 }
 
 let scaleMessage = generateScaleMessage(randScale);
@@ -58,7 +58,7 @@ console.log(getFinalMessage(scaleMessage, chordMessage, timeSigMessage))
 
 // const getMessage = (scale, chord, TS) => {
     // const typeOfMessage = [scale, chord, TS];
-    // let subject = typeOfMessage[randomizeNum(typeOfMessage)]
+    // let subject = typeOfMessage[getRandomNumber(typeOfMessage)]
     // if (subject === scale) {
         // return 
     // } 
