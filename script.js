4c9b7526c90962eac52d1824cd37d50863a1a9d5
Yonraz <yraz530@gmail.com>
2022-02-09 11:38:18 +0200

    Change possibleScales to scaleTones and make it global

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
+const scaleTones = ['C', 'D', 'E', 'F', 'G', 'A', 'B'];
 const generateScale = () =>{
-    const possibleScales = ['C', 'D', 'E', 'F', 'G', 'A', 'B'];
     const possibleTonality = ['Major', 'Minor', 'Phrygian', 'Lydian', 'Locrian', 'Mixolydian', 'Dorian'];
     function randomizeNum(){
         let randNum = Math.floor(Math.random()* 7);
         return randNum
     }
-    return possibleScales[randomizeNum()] + " " + possibleTonality[randomizeNum()]
+    return scaleTones[randomizeNum()] + " " + possibleTonality[randomizeNum()]
 }
+
