99ae3870ccf5799801d4fcaef9c5e5a8d853e6eb
Yonraz <yraz530@gmail.com>
2022-02-13 08:41:17 +0200

    minor changes

--- script.js
+++ script.js
@@ -1,26 +1,56 @@
-const scaleTones = ['C', 'D', 'E', 'F', 'G', 'A', 'B'];
-const possibleTonality = ['Major', 'Minor', 'Phrygian', 'Lydian', 'Locrian', 'Mixolydian', 'Dorian'];
-const possibleChordType = ['', 'maj', 'min', 'dim', 'aug'];
-const possibleTensions = ['7', 'maj7', 'maj9' , 'sus4', 'min9', '#11', 'alt7'];
-const possibleTimeSignature = ['4/4', '3/4', '5/4', '7/8'];
+const SCALE_TONES = [
+  'C',
+  'D',
+  'E',
+  'F',
+  'G',
+  'A',
+  'B'];
+const POSSIBLE_TONALITY = [ 
+  'Major', 
+  'Minor', 
+  'Phrygian', 
+  'Lydian', 
+  'Locrian', 
+  'Mixolydian', 
+  'Dorian'];
+const POSSIBLE_CHORD_TYPE = [ 
+  '', 
+  'maj', 
+  'min', 
+  'dim', 
+  'aug'];
+const POSSIBLE_TENSIONS = [
+  '7', 
+  'maj7', 
+  'maj9' ,
+  'sus4', 
+  'min9', 
+  '#11', 
+  'alt7'];
+const POSSIBLE_TIME_SIGNATURE = [
+  '4/4',
+  '3/4',
+  '5/4',
+  '7/8'];
 
 function getRandomNumber(array){
     let randNum = Math.floor(Math.random()* array.length);
     return randNum;
 }
 
-let chosenScale = scaleTones[getRandomNumber(scaleTones)];
-let chosenTonality = possibleTonality[getRandomNumber(possibleTonality)];
-let chosenChordType = possibleChordType[getRandomNumber(possibleChordType)];
-let chosenTension = possibleTensions[getRandomNumber(possibleTensions)];
-let chosenTimeSign = possibleTimeSignature[getRandomNumber(possibleTimeSignature)];
+let chosenScale = SCALE_TONES[getRandomNumber(SCALE_TONES)];
+let chosenTonality = POSSIBLE_TONALITY[getRandomNumber(POSSIBLE_TONALITY)];
+let chosenChordType = POSSIBLE_CHORD_TYPE[getRandomNumber(POSSIBLE_CHORD_TYPE)];
+let chosenTension = POSSIBLE_TENSIONS[getRandomNumber(POSSIBLE_TENSIONS)];
+let chosenTimeSign = POSSIBLE_TIME_SIGNATURE[getRandomNumber(POSSIBLE_TIME_SIGNATURE)];
 
 function generateScale() {
     return chosenScale + " " + chosenTonality;
 }
 
 function generateChord() { 
-    if(chosenChordType === ''){
+    if(chosenChordType === '') {
         return chosenScale +  chosenTension;
     }
     return chosenScale +  chosenChordType;
@@ -30,28 +60,40 @@ let randScale = generateScale();
 let randChord = generateChord();
 
 function generateScaleMessage(scale) {
-    const message = [`Your song is in the scale of ${scale}.`, `You better use ${scale} for this one.`, `${scale} is your scale for today.`];
+    const message = [ 
+        `Your song is in the scale of ${scale}.`,
+        `You better use ${scale} for this one.`,
+        `${scale} is your scale for today.`];
     return message[getRandomNumber(message)];
 }
 
 function generateChordMessage(chord) {
-    const message = [`Play ${chord}.`, `${chord} might be a good choice.`, `Try ${chord}! I swear it sounds good.`];
+    const message = [
+        `Play ${chord}.`,
+        `${chord} might be a good choice.`,
+        `Try ${chord}! I swear it sounds good.`];
     return message[getRandomNumber(message)];
 }
 
 function generateTSMessage(timeSig) {
-    const message = [`Your song should be in ${timeSig}... if you can handle it.`, `Maybe try doing it in ${timeSig} time`, `Your chosen time signature is ${timeSig}.`];
+    const message = [
+        `Your song should be in ${timeSig}... if you can handle it.`,
+        `Maybe try doing it in ${timeSig} time`,
+        `Your chosen time signature is ${timeSig}.`];
     return message[getRandomNumber(message)];
 }
 
 function modulate(randScale){
-    const message = [`Now go to the key of ${randScale}`, `Try moving up to ${randScale}`, `Mix it up with ${randScale}`]
+    const message = [
+        `Now go to the key of ${randScale}`,
+        `Try moving up to ${randScale}`,
+        `Mix it up with ${randScale}`]
     return message[getRandomNumber(message)];
 }
 
 function getFinalMessage(scaleFunc, chordFunc, TSFunc) {
     let possibleMessageArray = [scaleFunc, chordFunc, TSFunc];
-    let finalMessage = possibleMessageArray[getRandomNumber(possibleMessageArray)];
+    let finalMessage = possibleMessageArray[getRandomNumber(possibleMessageArray)]; 
     return finalMessage;
 }
 
